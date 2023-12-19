d634123a1ee406a3a6d9972bab7f3506c45292fa
GrantConnor <grentjconnor@gmail.com>
2023-12-19 11:26:00 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,26 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+});
+
+function submitForm() {
+    // Get form values
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    var dob = document.getElementById("dob").value;
+    var height = document.getElementById("height").value;
+    var weight = document.getElementById("weight").value;
+
+    // Basic form validation
+    if (name === "" || age === "" || dob === "" || height === "" || weight === "") {
+      alert("Please fill out all fields.");
+    } else {
+      // Display an alert when the form is submitted
+      alert("Thank you for signing up! We will message you shortly.");
+    }
+}
